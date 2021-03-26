cda2544dbaab495390ee80592194d116a3859799
Uk Chukundah <ukchukundah@gmail.com>
2021-03-26 21:26:24 +0100

    Move secret key to env variable

--- janitor/settings.py
+++ janitor/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '82behp90pey-40ect&$97n#uaj%@uiqo1^rgm%vd8#8&)#5t3^'
+SECRET_KEY = os.getenv('JANITOR_SECRET_KEY', 'secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('JANITOR_DEBUG', 'True') == 'True'
